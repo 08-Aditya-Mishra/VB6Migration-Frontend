9ab689eed4392b0ae9dbbb0177655da0ffaeb799
abhijitgunjal1648 <abhijit_gunjal@persistent.com>
2023-01-22 22:59:22 +0530

    integrated Atm and back with backend updated UI

--- src/ATM/MiniStatement.js
+++ src/ATM/MiniStatement.js
@@ -3,6 +3,7 @@ import './MiniStatement.css'
 import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import NotificationManager from 'react-notifications/lib/NotificationManager';
 
 const MiniStatement = () => {
 
@@ -11,39 +12,53 @@ const MiniStatement = () => {
     const location = useLocation();
     const navigate = useNavigate();
 
+    //Notifications
+    const showNotification = (type, message) => {
+        switch (type) {
+            case 'success':
+                NotificationManager.success(message);
+                break;
+            case 'warning':
+                NotificationManager.warning(message);
+                break;
+            case 'error':
+                NotificationManager.error(message);
+                break;
+            default:
+                NotificationManager.info(message);
+        }
+    }
+
 
     const handleClick = () => {
         navigate("/landing", { state: accountNo });
     }
-
+    var a = 0;
     useEffect(() => {
-        var accNo = location.state;
-        // alert(accNo)
-        setAccountNo(accNo);
-    }, [])
-
-    axios.get('http://localhost:5058/Transaction/' + accountNo)
-        .then(response => {
-            const accounts = response.data;
-            console.log(accounts)
-            if (accounts.length > 0) {
-                setTransactions(accounts);
-            } else {
-                // show an error message
-                // alert("Invalid account number: " + accountNo);
-            }
-        })
-        .catch(error => {
-            alert(error)
-            console.log(error);
-        });
+        var accountNo = location.state;
+        setAccountNo(accountNo);
+        axios.get('http://localhost:5058/Transaction/' + accountNo)
+            .then(response => {
+                const accounts = response.data;
+                if (accounts) {
+                    setTransactions(accounts);
+                    showNotification('success', 'Here are the recent transactions of your account');
+                } else {
+                    showNotification('error', 'Please enter an account number');
+                }
+            })
+            .catch(error => {
+                showNotification('error', 'Oops :(, Some error occured try again after some time');
+            });
+    }, [accountNo])
     
+
 
     return (
         <>
             <div className='mini-container'>
-                <header> <h2>MiniStatements</h2></header>
-                <header> <h3>Account Number: {accountNo}</h3></header>
+                <div className='miniHeader'> <h2>MiniStatements</h2></div>
+                <div className='miniHeader'> <h3>Account Number: {accountNo}</h3></div>
                 <div className="col-sm-6 table-responsive mini-statement-container">
                     <table class="table table-striped">
                         <thead>
@@ -65,7 +80,7 @@ const MiniStatement = () => {
                     </table>
                 </div>
                 <div className="back-button">
-                    <button className="btn btn-dark" onClick={handleClick}>Back</button>
+                    <button className="btn btn-dark" onClick={handleClick}>Close</button>
                 </div>
             </div>
         </>
