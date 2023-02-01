060f87ed254985068147c967edc06f6325fc9c0c
Aditya Mishra <84185824+08-Aditya-Mishra@users.noreply.github.com>
2023-02-01 22:17:56 +0530

    Updated Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -15,7 +15,7 @@ function Navbar() {
 
             method: "delete",
 
-            url: "http://localhost:5178/api/Admin/logout",
+            url: "https://localhost:7286/api/Admin/logout",
 
             headers: {
                 Authorization: `bearer ${localStorage.getItem("accessToken")}`,
@@ -33,31 +33,27 @@ function Navbar() {
 
 
     return (
-        <div className="navbar-bg" >
-            <nav class="navbar navbar-expand-lg" style={{ backgroundColor: "#00003f" }}>
-                <div class="container-fluid">
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item" >
-                                <a class="nav-link active" style={{color: 'white'}} href="/" aria-current="page">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" style={{color: 'white'}} href="/createaccount">Add New Account</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" style={{color: 'white'}} href="/deposit">Deposit</a>
-                            </li>
+        <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <li class="nav-item" >
+    <a class="nav-link active" style={{color: 'white'}} href="/" aria-current="page">Home</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link active" style={{color: 'white'}} href="/createaccount">Add New Account</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link active" style={{color: 'white'}} href="/deposit">Deposit</a>
+    </li>
                             {/* <li class="nav-item">
                                 <a class="nav-link disabled" style={{color: 'white'}} href="#">Update port</a>
                             </li> */}
-                        </ul>
-                        <span class="navbar-text">
-                            <button onClick={handleLogout} class="btn btn-outline-light" type="submit">Log Out</button>
-                        </span>
-                    </div>
-                </div>
-            </nav>
-        </div>
+    </ul>
+    <span class="navbar-text">
+    <button onClick={handleLogout} class="btn btn-outline-light" type="submit">LOG OUT</button>
+    </span>
+  </div>
+</nav>
     )
 }
 export default Navbar;
